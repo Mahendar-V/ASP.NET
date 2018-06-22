dd53a2b2df05f153b989be8a48915ca2348a9ae9
VANAPARTHI MAHENDAR <vanaparthimahendar@yahoo.com>
2018-06-22 11:06:17 +0530

    Add project files.

---
+++ HttpModules/CustomModule.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Web;
+
+namespace HttpModules
+{
+    public class CustomModule : IHttpModule
+    {
+        private DateTime RequestStartAt { get; set; }
+        public void Dispose()
+        {
+           
+        }
+
+        public void Init(HttpApplication context)
+        {
+            context.BeginRequest += OnBeginEachRequest;
+            context.EndRequest += OnEndEachRequest;
+        }
+
+        private void OnEndEachRequest(object sender, EventArgs e)
+        {
+            HttpApplication httpApp = sender as HttpApplication;
+            httpApp.Response.Write("Request End :" + DateTime.Now);
+        }
+
+        private void OnBeginEachRequest(object sender, EventArgs e)
+        {
+            HttpApplication httpApp = sender as HttpApplication;
+            httpApp.Response.Write("Browser : "+ httpApp.Request.Browser.Type +"</br>");
+            httpApp.Response.Write("Request Start :" + DateTime.Now);
+            RequestStartAt = DateTime.Now;
+        }
+    }
+}
